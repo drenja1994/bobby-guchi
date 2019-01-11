c2796b9414b7fdf64c48183e8a5836f077c28502
m <m@DESKTOP-5BUBDGC>
2019-01-11 14:05:45 +0100

    1st Commit - Work on Role.php.Making CRUDS functions

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -9,4 +9,11 @@ class Role {
 		$rezultat = DB::table('role')->get();
 		return $rezultat;
 	}
+        
+        public function getRolesByUser($user_id){
+		$rezultat = DB::table('user_role')->get();
+		return $rezultat;
+	}
+        
+        
 }
