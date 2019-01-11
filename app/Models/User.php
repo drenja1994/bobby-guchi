7293e5d718d35c27d86ac506d0e0bc6f1329e4c7
Drenja <Korisnik@109.122.126.112>
2019-01-11 13:41:25 +0100

    complete adjustments of User model, to incorporate fields from new DB

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,33 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: korisnik
- * Date: 04-Feb-18
- * Time: 21:47
- */
+
 
 namespace App\Models;
 
 class UserModel
 {
-    public $korisnicko_ime;
-    public $prezime;
-    public $email;
-    public $password;
+    public $firstname;
+    public $lastname;
     public $username;
-    public $uloga_id;
+    public $password;
+    public $email;
+    public $ranking_id;
     private $table = 'user';
 
     public function insert()
     {
         return \DB::table($this->table)
             ->insertGetId([
-                'korisnicko_ime' => $this->korisnicko_ime,
-                'prezime' => $this->prezime,
+                'firstname' => $this->firstname,
+                'lastname' => $this->lastname,
                 'username' => $this->username,
                 'password' => md5($this->password),
                 'email' => $this->email,
-                'uloga_id' => $this->uloga_id,
+                'ranking_id' => $this->ranking_id,
             ]);
     }
 
@@ -54,12 +49,12 @@ class UserModel
         return \DB::table($this->table)
             ->where("id", $id)
             ->update([
-                'korisnicko_ime' => $this->korisnicko_ime,
-                'prezime' => $this->prezime,
+                'firstname' => $this->firstname,
+                'lastname' => $this->lastname,
                 'password' => md5($this->password),
                 'email' => $this->email,
                 'username' => $this->username,
-                'uloga_id' => $this->uloga_id
+                'ranking_id' => $this->ranking_id_id
             ]);
     }
 
